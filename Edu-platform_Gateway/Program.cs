965aeffd4a961b5bbe758fda06321ba3c5ac7229
Nazaziik <nazar240902@gmail.com>
2024-01-05 11:17:03 -0600

    gateway fixed and tested

--- Edu-platform_Gateway/Program.cs
+++ Edu-platform_Gateway/Program.cs
@@ -5,7 +5,7 @@ using Ocelot.Middleware;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Configuration.SetBasePath(builder.Environment.ContentRootPath)
-    .AddJsonFile($"ocelot.{builder.Environment}.json", optional: false, reloadOnChange: true)
+    .AddJsonFile($"ocelot.{builder.Environment.EnvironmentName}.json", optional: false, reloadOnChange: true)
     .AddEnvironmentVariables();
 
 builder.Services.AddOcelot(builder.Configuration);
